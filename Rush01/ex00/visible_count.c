dac84816950285e2ef192ae6a8fcfe2fa3ff6a48
Eliott Procureur <elprocur@fu-r2-p4.s19.be>
2026-08-15 13:32:12 +0200

    adpat to size

--- Rush01/ex00/visible_count.c
+++ Rush01/ex00/visible_count.c
@@ -1,24 +1,29 @@
-int visible_count(int *sol)
-{
-    int i = 1;
-    int max = sol[0];
-    int count = 1;
+#include "rush01.h"
 
-    while (i < 4)
+int visible_count(int *sol, int SIZE)
+{
+    int i;
+    int max;
+    int count;
+
+    i = 1;
+    max = sol[0];
+    count = 1;
+    while (i < SIZE)
     {
         if (sol[i] > max)
-        {
+        {   
             max = sol[i];
             count++;
         }
         i++;
     }
-    return count;
+    return (count);
 }
 
 /*
     Cette fonction compte combien de nombres sont visibles en partant du début d'une ligne ou colonne. Elle prend en paramètre 
-    un tableau de 4 entiers représentant une ligne ou une colonne et retourne le nombre de batiments visibles.
+    un tableau de 'size' entiers représentant une ligne ou une colonne et retourne le nombre de batiments visibles.
     Exemple :
     sol = {2, 3, 1, 4}
     visible_count(sol, 4) -> 3
